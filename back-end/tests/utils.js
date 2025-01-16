8fb2d0fd820d07b2577f753361f001688767e259
prabowo-sakti <prabowosakti2023@gmail.com>
2025-01-16 12:30:01 +0700

    mengubah testing menjadi mongodb untuk sebagian

--- back-end/tests/utils.js
+++ back-end/tests/utils.js
@@ -1,12 +1,42 @@
-import { writeFileSync } from 'node:fs'
-import { join } from 'node:path'
+import mongoose from "mongoose";
+import { Whisper } from "../database.mjs";
 
-const dbPath = join(process.cwd(), 'db.json')
-const restoreDb = () => {
-  return writeFileSync(dbPath, JSON.stringify([]))
-}
-const populateDb = (data) => {
-  return writeFileSync(dbPath, JSON.stringify(data))
-}
+const ensureDbConnection = async () => {
+  try {
+    if (mongoose.connection.readyState !== 1) {
+      await mongoose.connect(process.env.MONGODB_URI);
+    }
+  } catch (err) {
+    console.error("Error connecting to the database", err);
+    throw err;
+  }
+};
 
-export { restoreDb, populateDb }
+const closeDbConnection = async () => {
+  if (mongoose.connection.readyState === 1) {
+    await mongoose.disconnect();
+  }
+};
+
+const restoreDb = () => Whisper.deleteMany({});
+const populateDb = () =>
+  Whisper.insertMany([{ message: "test" }, { message: "hello world" }]);
+
+const getFixtures = async () => {
+  const data = await Whisper.find();
+  const whispers = JSON.parse(JSON.stringify(data));
+  const inventedId = "64e0e5c75a4a3c715b7c1074";
+  const existingId = data[0].id;
+  return { inventedId, existingId, whispers };
+};
+
+const normalize = (data) => JSON.parse(JSON.stringify(data));
+
+export {
+  restoreDb,
+  populateDb,
+  getFixtures,
+  ensureDbConnection,
+  normalize,
+  closeDbConnection,
+};
