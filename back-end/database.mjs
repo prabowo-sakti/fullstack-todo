6a3de82e1737e97c1435fda74be33f6b496b118c
prabowo-sakti <prabowosakti2023@gmail.com>
2025-01-26 16:12:23 +0700

    perubahan besar terhadap database.mjs, store.mjs, dsb

--- back-end/database.mjs
+++ back-end/database.mjs
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import validator from "validator";
 import bcrypt from "bcrypt";
+import { checkPasswordStreng } from "./utils";
 
 mongoose.set("toJSON", {
   virtuals: true,
@@ -15,6 +16,19 @@ const userSchema = new mongoose.Schema({
     type: String,
     unique: true,
     trim: true,
+    required: [true, "Username is required"],
+    minlength: [3, "Username must be at least 3 characters long"],
+    maxlength: [20, "Username must be at most 20 characters long"],
+  },
+
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minlength: [8, "Pasword must be at least 8 characters long"],
+    validate: {
+      validator: checkPasswordStreng,
+      prop
+    },
   },
 });
 
